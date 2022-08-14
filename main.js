76337d87cdf758defc5a4d95c600a1d2fb5d9e58
Kasper Westeraa <kwe@delegate.dk>
2022-08-14 16:13:15 +0200

    CheckInput() implemented

---
+++ main.js
@@ -0,0 +1,11 @@
+let fornavn = document.getElementById("fname");
+let efternavn = document.getElementById("lname");
+let button = document.getElementById("button");
+
+function checkInput() {
+  if (fornavn.value == "" || efternavn.value == "") {
+    button.style.opacity = 0.4;
+  } else {
+    button.style.opacity = 1;
+  }
+}
