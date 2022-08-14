2b577c8da91f328dcc6de0e2c94e951ed6f138e1
Kasper Westeraa <kwe@delegate.dk>
2022-08-14 23:18:08 +0200

    onSubmit() implemented

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let fornavn = document.getElementById("fname");
-let efternavn = document.getElementById("lname");
+let fornavnElement = document.getElementById("fname");
+let efternavnElement = document.getElementById("lname");
 let button = document.getElementById("button");
 
 function checkInput() {
-  if (fornavn.value !== "" && efternavn.value !== "") {
+  if (fornavnElement.value !== "" && efternavnElement.value !== "") {
     button.style.opacity = 1;
   } else {
     button.style.opacity = 0.4;
   }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log("Submitted");
 }
